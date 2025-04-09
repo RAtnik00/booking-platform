cd8b06caccdc63c79dbe308e75bcc61b4cd3a54e
RAtnik00 <yaremenkodmitriy22@gmail.com>
2025-04-09 10:58:42 +0200

    Create admin file

--- tutor_booking/accounts/admin.py
+++ tutor_booking/accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    models = CustomUser
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_active')
+    fieldsets = UserAdmin.fieldsets + (
+        ('More info', {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('More info', {'fields': ('role',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
