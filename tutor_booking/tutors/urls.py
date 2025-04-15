c09b6021fbc3e6bf4f77dc293bfc133578a7efeb
RAtnik00 <yaremenkodmitriy22@gmail.com>
2025-04-15 19:01:29 +0200

    add path

--- tutor_booking/tutors/urls.py
+++ tutor_booking/tutors/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import tutor_list
+from .views import tutor_list, tutor_detail
 
 urlpatterns = [
     path('tutors/', tutor_list, name='tutor_list'),
+    path('tutors/<slug:slug>/', tutor_detail, name='tutor_detail')
 ]
