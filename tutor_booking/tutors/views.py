304c49b738d2bb6bd68ce339f2a089f504c4e57f
RAtnik00 <yaremenkodmitriy22@gmail.com>
2025-04-16 17:06:58 +0200

    add code

--- tutor_booking/tutors/views.py
+++ tutor_booking/tutors/views.py
@@ -2,10 +2,24 @@ from accounts.models import CustomUser
 from django.shortcuts import render, get_object_or_404
 
 def tutor_list(request):
-    tutors = CustomUser.objects.filter(role='tutor')
+    filters = {}
+    subject = request.GET.get('subject')
+    if subject:
+        filters['tutor_profile__subjects__icontains'] = subject
+
+    max_price = request.GET.get('max_price')
+    if max_price:
+        filters['tutor_profile__price_per_hour__lte'] = max_price
+
+    q = request.GET.get('q')
+    if q:
+        filters['username__icontains'] = q
+
+    tutors = CustomUser.objects.filter(role='tutor', **filters)
+
+    print('FILTERS:', filters)
     print('TUTORS:', tutors)
-    for t in tutors:
-        print(t.username, hasattr(t, 'tutor_profile'), t.tutor_profile.subjects)
+
     return render(request, 'tutors/tutor_list.html', {'tutors': tutors})
 
 def tutor_detail(request, slug):
