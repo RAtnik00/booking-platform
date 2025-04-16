5201b8721fef62f18af24639948bb167acbc8afe
RAtnik00 <yaremenkodmitriy22@gmail.com>
2025-04-16 22:16:58 +0200

    fix bug

--- tutor_booking/accounts/models.py
+++ tutor_booking/accounts/models.py
@@ -31,17 +31,3 @@ class TutorProfile(models.Model):
     subjects = models.CharField(max_length=255, blank=True, default="")
     price_per_hour = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
 
-class Booking(models.Model):
-    STATUS_CHOICES = (
-        ('pending', 'Pending'),
-        ('confirmed', 'Confirmed'),
-        ('cancelled', 'Cancelled'),
-    )
-
-    student = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE, related_name='student_booking')
-    tutor = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE, related_name='tutor_booking')
-    datetime = models.DateTimeField(blank=True, null=True)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
-
-    def __str__(self):
-        return f"{self.student.username} -> {self.tutor.username} @ {self.datetime}"
--- tutor_booking/tutors/models.py
+++ tutor_booking/tutors/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Booking(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('confirmed', 'Confirmed'),
+        ('cancelled', 'Cancelled'),
+    )
+
+    student = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE, related_name='student_booking')
+    tutor = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE, related_name='tutor_booking')
+    datetime = models.DateTimeField(blank=True, null=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+
+    def __str__(self):
+        return f"{self.student.username} -> {self.tutor.username} @ {self.datetime}"
