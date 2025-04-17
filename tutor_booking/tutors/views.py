34e04762c239beb2116e66ed0229fe171642a27f
RAtnik00 <yaremenkodmitriy22@gmail.com>
2025-04-17 09:06:12 +0200

    add new code

--- tutor_booking/tutors/views.py
+++ tutor_booking/tutors/views.py
@@ -1,5 +1,7 @@
 from accounts.models import CustomUser
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from .models import Booking
 
 def tutor_list(request):
     filters = {}
@@ -24,5 +26,17 @@ def tutor_list(request):
 
 def tutor_detail(request, slug):
     tutor = get_object_or_404(CustomUser, slug=slug, role = 'tutor')
+
+    if request.method == 'POST':
+        datetime_value = request.POST.get('datetime')
+        if request.user.is_authenticated and request.user.role == 'student':
+            Booking.objects.create(
+                student=request.user,
+                tutor=tutor,
+                datetime=datetime_value,
+                status='pending'
+            )
+            return HttpResponseRedirect(request.path_info)
+
     context = {'tutor': tutor}
     return  render(request, 'tutors/tutor_detail.html', context)
