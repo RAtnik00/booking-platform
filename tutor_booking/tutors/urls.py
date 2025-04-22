ee1c7f03267b4c15f3a49aa0f2d127f22724c03c
RAtnik00 <yaremenkodmitriy22@gmail.com>
2025-04-22 17:46:12 +0200

    add new web page

--- tutor_booking/tutors/urls.py
+++ tutor_booking/tutors/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import tutor_list, tutor_detail
+from .views import tutor_list, tutor_detail, my_bookings
 
 urlpatterns = [
     path('tutors/', tutor_list, name='tutor_list'),
-    path('tutors/<slug:slug>/', tutor_detail, name='tutor_detail')
+    path('tutors/<slug:slug>/', tutor_detail, name='tutor_detail'),
+    path('my-bookings/', my_bookings, name='my_bookings')
 ]
