8e006b92a1376922e09aa317ea2d8873cec039c9
RAtnik00 <yaremenkodmitriy22@gmail.com>
2025-04-23 10:24:53 +0200

    fix error

--- tutor_booking/tutors/views.py
+++ tutor_booking/tutors/views.py
@@ -69,7 +69,7 @@ def cancel_booking(request, booking_id):
     booking = get_object_or_404(Booking, id=booking_id)
 
     if request.user.is_authenticated and request.user.role == 'student' and booking.student == request.user:
-        booking.student = 'cancelled'
+        booking.status = 'cancelled'
         booking.save()
 
     return HttpResponseRedirect('/my-bookings/')
