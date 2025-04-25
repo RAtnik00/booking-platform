dd226da41bb98eee90d51b5163ac7970d440ec39
RAtnik00 <yaremenkodmitriy22@gmail.com>
2025-04-25 08:50:30 +0200

    add view for accounts

--- tutor_booking/accounts/views.py
+++ tutor_booking/accounts/views.py
@@ -1,1 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from accounts.forms import UserRegistrationForm
+from django.contrib.auth.hashers import make_password
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.password = make_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
+
