afa194185221f75a3ddb7eafffd596b0428e2182
RAtnik00 <yaremenkodmitriy22@gmail.com>
2025-04-29 16:33:45 +0200

    fix bug

--- tutor_booking/accounts/urls.py
+++ tutor_booking/accounts/urls.py
@@ -6,5 +6,5 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='accounts/logged_out.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/accounts/login/'), name='logout'),
 ]
